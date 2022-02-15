b57e110221f637449b00f221a5534b38adf12929
Maxim Yefremov <maxYefr@gmail.com>
2022-02-15 12:59:25 +0300

    func

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ypapax/cpu_usage
+
+go 1.17
+
+require github.com/pkg/errors v0.9.1 // indirect
--- main.go
+++ main.go
@@ -1,8 +1,9 @@
- //https://stackoverflow.com/a/11357813/1024794
+//https://stackoverflow.com/a/11357813/1024794
 package main
 
 import (
 	"bytes"
+	"github.com/pkg/errors"
 	"log"
 	"os/exec"
 	runtime "runtime"
@@ -17,6 +18,15 @@ type Process struct {
 }
 
 func main() {
+	percent, err := cpuUsage()
+	if err != nil {
+		log.Printf("error: %+v\n", err)
+		return
+	}
+	log.Println("cpu usage: ", percent)
+}
+
+func cpuUsage() (float64, error) {
 	t1 := time.Now()
 	log.SetFlags(log.Llongfile | log.LstdFlags)
 	cmd := exec.Command("ps", "aux")
@@ -24,38 +34,40 @@ func main() {
 	cmd.Stdout = &out
 	err := cmd.Run()
 	if err != nil {
-		log.Fatal(err)
+		return 0, errors.WithStack(err)
 	}
 	processes := make([]*Process, 0)
 	for {
-		line, err := out.ReadString('\n')
-		if err!=nil {
-			break;
+		line, errR := out.ReadString('\n')
+		if errR != nil {
+			break
 		}
 		tokens := strings.Split(line, " ")
 		ft := make([]string, 0)
-		for _, t := range(tokens) {
-			if t!="" && t!="\t" {
+		for _, t := range tokens {
+			if t != "" && t != "\t" {
 				ft = append(ft, t)
 			}
 		}
 		log.Println(len(ft), ft)
-		pid, err := strconv.Atoi(ft[1])
-		if err!=nil {
+		pid, errR := strconv.Atoi(ft[1])
+		if errR != nil {
 			continue
 		}
-		cpu, err := strconv.ParseFloat(ft[2], 64)
-		if err!=nil {
-			log.Fatal(err)
+		cpu, errR := strconv.ParseFloat(ft[2], 64)
+		if errR != nil {
+			return 0, errors.WithStack(err)
 		}
 		processes = append(processes, &Process{pid, cpu})
 	}
 	var cpuSum float64
-	for _, p := range(processes) {
-		log.Println("Process ", p.pid, " takes ", p.cpu, " % of the CPU")
-		cpuSum+=p.cpu
+	for _, p := range processes {
+		//log.Println("Process ", p.pid, " takes ", p.cpu, " % of the CPU")
+		cpuSum += p.cpu
 	}
 	log.Printf("sum: %+v, time spent: %+v \n", cpuSum, time.Since(t1))
 	log.Println("The number of CPU Cores:", runtime.NumCPU())
-	log.Println("cpu usage: ", cpuSum / float64(runtime.NumCPU()))
+	usage := cpuSum/float64(runtime.NumCPU())
+	log.Println("cpu usage: ", usage)
+	return usage, nil
 }
